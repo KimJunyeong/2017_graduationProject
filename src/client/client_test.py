96c8de5a99b3c8825fbfdcd6d7c3cd9269eb0b17
KimJunyeong <jurywnsdud@gmail.com>
2017-07-21 14:01:10 +0900

    Create client_test.py

---
+++ src/client/client_test.py
@@ -0,0 +1,13 @@
+import socket
+from datetime import datetime
+
+address = (192.168.1.17, 12345)
+max_size = 1024
+
+print('starting the client at', datetime.now())
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(address)
+client.sendall('hey')
+data = client.recv(max_size)
+print('At', datetime.now(), 'someone replied', data)
+client.close()
