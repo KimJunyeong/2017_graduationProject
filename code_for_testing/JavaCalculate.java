7c235bb7caa8b74aeaeb0dce837158ab53a4a0be
rapunzel1123 <rapunzel1123@naver.com>
2017-09-13 14:40:08 +0900

    Update JavaCalculate.java

--- code_for_testing/JavaCalculate.java
+++ code_for_testing/JavaCalculate.java
@@ -5,26 +5,38 @@ public class JavaCalculate {
 	public static void findNode(String line){
 		String[] array = null;
         String[] name_value = null;
-        double[] intArray = new double[6];
+        double[] intArray = new double[7];
         
+        //reset node
+        /*
         double node[][] = {
-				{76.35, 78.55, 90.75, 88.2,  88.75, 92.45},
-				{78.9,  78.7,  83.15, 87.05, 88.6,  90.45},
-				{87.3,  80.95, 74.35, 92.35, 91.3,  89.4 },
-				{87.25, 94.25, 96.59, 73.65, 79.9,  92.82},
-				{92.74, 94.73, 94.75, 87.4,  74.7,  77.2 },
-				{97,    97.67, 94,    94.5,  90.75, 69.6 },
-				{85.3,  89.9,  92.2,  83.3,  86.35, 94.4 },
-				{82.85, 86,    91.05, 86.65, 81.05, 86.85},
-				{87.85, 91.75, 84.4,  89.7,  87.25, 84.05},
-				{94,    92.5,  95.8,  97.62, 93.1,  91.05}
+				{-76.35, -78.55, -90.75, -88.2,  -88.75, -92.45},
+				{-78.9,  -78.7,  -83.15, -87.05, -88.6,  -90.45},
+				{-87.3,  -80.95, -74.35, -92.35, -91.3,  -89.4 },
+				{-87.25, -94.25, -96.59, -73.65, -79.9,  -92.82},
+				{-92.74, -94.73, -94.75, -87.4,  -74.7,  -77.2 },
+				{-97,    -97.67, -94,    -94.5,  -90.75, -69.6 },
+				{-85.3,  -89.9,  -92.2,  -83.3,  -86.35, -94.4 },
+				{-82.85, -86,    -91.05, -86.65, -81.05, -86.85},
+				{-87.85, -91.75, -84.4,  -89.7,  -87.25, -84.05},
+				{-94,    -92.5,  -95.8,  -97.62, -93.1,  -91.05}
 		};
+		*/
+        double node[][] = {
+        		{-78.95,-88.8,-92.45,-88.1,-95.15,-105,-96.3},
+        		{-90.05,-77.05,-84.8,-93.3,-89.55,-104.25,-90.2},
+        		{-93.2,-85.65,-82.15,-94.9,-89.75,-103.2,-79.15},
+        		{-89.6,-95.15,-101.7,-75.45,-85.85,-103.75,-100.9},
+        		{-103.05,-97.9,-98.35,-83.5,-85.05,-86.8,-102.8},
+        		{-104.7,-105,-100.05,-93.9,-88.75,-82.4,-91.4},
+        		{-102.75,-95.3,-92.85,-100.9,-90.55,-94.3,-83.1}
+        };
         
         array = null;
         array = line.replaceAll("null", "").split(",");
 
-        int diff[][] = new int[10][6];					//10 node, 6 beacon signal value
-        int tdiff[] = {0,0,0,0,0,0,0,0,0,0};			//total difference for each node value
+        double diff[][] = new double[7][7];					//10 node, 6 beacon signal value
+        double tdiff[] = {0,0,0,0,0,0,0};			//total difference for each node value
 		int minIndex;									//smallest tdiff value = closest node
 		
         for(int i = 0;i < array.length;i++) {
@@ -46,8 +58,11 @@ public class JavaCalculate {
                 case "dog":
                     intArray[4] = Integer.parseInt(name_value[1]);
                     break;
-                case "door":
+                case "fridge":
                     intArray[5] = Integer.parseInt(name_value[1]);
+                    break;
+                case "chair":
+                    intArray[6] = Integer.parseInt(name_value[1]);
                     break;
                 default:
                     break;
@@ -55,13 +70,13 @@ public class JavaCalculate {
         }
         dumpArray(intArray);//
         
-        for(int i=0;i<10;i++){
+        for(int i=0;i<7;i++){
         	tdiff[i] = 0;
-			for(int j=0;j<6;j++){
+			for(int j=0;j<7;j++){
 				if(intArray[j] == 0){
-					intArray[j] = -100;
+					intArray[j] = -105;
 				}
-				int temp = (int) (node[i][j] + intArray[j]);
+				int temp = (int) (node[i][j] - intArray[j]);
 				diff[i][j] = (int) Math.pow(temp, 2);
 				tdiff[i] += diff[i][j];
 			}
@@ -70,21 +85,16 @@ public class JavaCalculate {
         
         minIndex = 0;
         
-	    for (int i = 0; i < 10; i++) {
+	    for (int i = 0; i < 7; i++) {
 	    	double newnumber = tdiff[i];
 	        if ((newnumber < tdiff[minIndex])) {
 	            minIndex = i;
 	        }
 	    }
 	    
-	    if(minIndex>5 && minIndex<9){		//node 7 8 9
-	    	//JavaClient.sendNode(7);
-	    } else{
-		    //JavaClient.sendNode(minIndex+1);	
-	    }
-	    
 	    //JavaDatabase.storeData(minIndex+1);
 	    //JavaDatabase.decideDanger(minIndex+1);
+	    //JavaDatabase.storeBeaconData(intArray);
 	}
 	public static void dumpArray(double[] array) {
 	    for (int i = 0; i < array.length; i++)
